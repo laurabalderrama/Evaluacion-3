1c03bda508131fcdceefeb0edaf4c3b32d7e3ff0
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-04-27 08:28:28 -0600

    Add files via upload

---
+++ EVA3_1_UNCHEKED_EXEPTIONS/src/eva3_1_uncheked_exeptions/EVA3_1_UNCHECKED_EXCEPTIONS.java
@@ -0,0 +1,40 @@
+package eva3_1_uncheked_exeptions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_UNCHECKED_EXCEPTIONS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*System.out.println("INICIANDO EL PROGRAMA");
+        System.out.println("DECLARANDO VARIABLES");
+        int x = 5, y = 0;
+        System.out.println("INTEBTAR LA DIVISIÓN ");
+        int resu = x / y;
+        System.out.println("Resultado: " + resu*/
+        
+        //INPUT
+        /*Scanner input = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int nume = input.nextInt();// si el usuario captura una cadena que no puede convertirse a numero se produce una excepción
+        System.out.println("El número es: " + nume); */       
+            
+        //ARRAYINDEXOUTOFBOUNDS EXCEPTION
+       /* int[] arreglo = new int[5];
+        arreglo[0] = 100;
+        arreglo[1] = 200;
+        arreglo[2] = 300;
+        arreglo[3] = 400;
+        arreglo[4] = 500;
+        arreglo[5] = 600;*/
+        // el arreglo va de 0 a 4
+    }
+    
+}
